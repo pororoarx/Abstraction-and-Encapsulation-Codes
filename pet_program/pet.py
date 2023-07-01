2036ab6a83ef26d9960b30c8014f848408ff65bc
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 18:11:45 +0800

    created three getter methods for 3 properties

--- pet_program/pet.py
+++ pet_program/pet.py
@@ -12,12 +12,21 @@ class Pet:
 
     # setter method for animal type
     def set_anime_type(self, animal_type):
-        self.__anime_type = animal_type
+        self.__animal_type = animal_type
 
     # setter method for age
     def set_age(self, age):
         self.__age = age
 
     # getter method for name
+    def get_name(self):
+        return self.__name
+    
     # getter method for animal type
+    def get_animal_type(self):
+        return self.__animal_type
+    
     # getter method for age
+    def get_age(self):
+        return self.__age
+    
