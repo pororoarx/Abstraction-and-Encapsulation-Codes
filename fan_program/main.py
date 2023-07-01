2dc1b73a64c0b29391e0510e07a829d36ff0228f
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 13:42:33 +0800

    wrote pseudocode for the main file

---
+++ fan_program/main.py
@@ -0,0 +1,11 @@
+# import Fan class and all the properties
+
+
+# set the properties for fan 1
+# create instances of the classes for fan 1
+# display fan 1
+
+
+# set the properties for fan 2
+# create instances of the classes for fan 2
+# display fan 2
