97c31f169591c24712d970d8cc8af761cdb2f590
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 16:19:26 +0800

    created car class and private members

--- car_program/car.py
+++ car_program/car.py
@@ -1,6 +1,10 @@
 # create Car class
-
+class Car:
+    def __init__(self, year_model, make):
         # private members
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
 
     # accelerate method
     # brake method
