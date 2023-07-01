537353a2ef922513e2d9296e9350584d1937c5ad
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 17:58:30 +0800

    created 3 methods for the three properties

--- fan_program/speed.py
+++ fan_program/speed.py
@@ -6,8 +6,8 @@ class Speed:
 
     # getter method
     def get_speed(self):
-        return self.__fan.__speed
+        return self.__fan._speed
 
     # setter method
     def set_speed(self, speed):
-        self.__fan.__speed = speed
+        self.__fan._speed = speed
--- pet_program/pet.py
+++ pet_program/pet.py
@@ -6,10 +6,17 @@ class Pet:
         self.__animal_type = animal_type
         self.__age = age
 
-    
     # setter method for name
+    def set_name(self, name):
+        self.__name = name
+
     # setter method for animal type
+    def set_anime_type(self, animal_type):
+        self.__anime_type = animal_type
+
     # setter method for age
+    def set_age(self, age):
+        self.__age = age
 
     # getter method for name
     # getter method for animal type
