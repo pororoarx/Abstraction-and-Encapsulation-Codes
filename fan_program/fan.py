d49bbd7519cca2af0cdf4ae8052524e2fb2566e7
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 13:47:03 +0800

    Initialized the properties of the fan

--- fan_program/fan.py
+++ fan_program/fan.py
@@ -6,3 +6,8 @@ class Fan:
     FAST =3
 
     # initialize the properties of the fan
+    def __init__(self, speed = SLOW, radius = 5, color = "blue", on = False):
+        self._speed = speed
+        self._radius = radius
+        self._color =  color
+        self._on = on
