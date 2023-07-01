923e3fc8b5361dab75ee7379d62228cb7b4a61e8
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 17:44:02 +0800

    wrote pseudocode for pet.py file

---
+++ pet_program/pet.py
@@ -0,0 +1,12 @@
+# create Pet class
+
+        # private members
+
+    
+    # setter method for name
+    # setter method for animal type
+    # setter method for age
+
+    # getter method for name
+    # getter method for animal type
+    # getter method for age
