3382ac4b70583a96ca614b5ed3844c40de78af94
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 18:33:05 +0800

    ask user to input their pet's name, type, age

--- pet_program/main.py
+++ pet_program/main.py
@@ -2,6 +2,9 @@
 from pet import Pet
 
 # ask user for input: name, animal_type, age
+name = input("\nEnter the pet's name: ")
+animal_type = input("Enter the type of your pet: ")
+age = input("Enter the pet's age: ")
 
 # create instance of the class (Pet)
 
