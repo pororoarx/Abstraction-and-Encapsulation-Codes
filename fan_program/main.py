1f7cd2c59090418798cc576b35e4ad814fe1811e
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 14:16:00 +0800

    created the remaining instances of the class for fan 1

--- fan_program/main.py
+++ fan_program/main.py
@@ -10,6 +10,9 @@ fan_1 = Fan(Fan.FAST, 10, "yellow", True)
 
 # create instances of the classes for fan 1
 speed_1 = Speed(fan_1)
+radius_1 = Radius(fan_1)
+color_1 = Color(fan_1)
+on_1 = On(fan_1)
 
 # display fan 1
 print("Speed:", speed_1.get_speed())
