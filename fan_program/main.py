e007ea10dd54a05cf71699bec3c0fb0e0a3f2ca1
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 14:18:49 +0800

    printed remaining properties for fan 1

--- fan_program/main.py
+++ fan_program/main.py
@@ -16,6 +16,9 @@ on_1 = On(fan_1)
 
 # display fan 1
 print("Speed:", speed_1.get_speed())
+print("Radius:", radius_1.get_radius())
+print("Color:", color_1.get_color())
+print("On properties (True- On, False- Off):", on_1.get_on())
 
 
 # set the properties for fan 2
