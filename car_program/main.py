593af9293521528cc5e3ebfd1c26036e151fcae7
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-01 16:34:48 +0800

    wrote pseudocode for main file

---
+++ car_program/main.py
@@ -0,0 +1,18 @@
+# import class from car
+
+# define a function
+    # create an instance of a class and set the car's model and make
+
+    # print accelerates text
+
+    # use a for loop to call the accelerate method five times
+        # display the current speed
+
+
+    # print brakes text
+
+    # use a for loop to call the brake method five times
+        # display the current speed
+
+
+# call the function
